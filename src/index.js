cc2b5156b55c231a53d7f07001b0b7d658582943
Hani J <hanijandali@gmail.com>
2022-07-21 20:38:38 -0700

    Props and children of props

--- src/index.js
+++ src/index.js
@@ -1,8 +1,48 @@
 import React from 'react';
-import ReactDom from 'react-dom'
+import ReactDom from 'react-dom';
+import './index.css'
 
-function Greeting(){
-  return <h4>This is the first component</h4>
+//Set Up variables
+
+const firstBook = {
+  author: 'Amelia Airheart',
+  title: 'I love You to the moon and back',
+  publisher: 'Penguin House Publishing',
+  job:'Writer'
 }
 
-ReactDom.render(<Greeting/>, document.getElementById('root'));
+const secondBook = {
+  author: 'F Scott Fitz',
+  title: 'Summers Paradise',
+  publisher: 'Penguin House Publishing',
+  job:'Artist'
+}
+
+function BookList(){
+  return (
+    <section className='booklist'>
+    <Book title={firstBook.title} author={firstBook.author}>
+      <p> Lorem ipsum dolor sit amet whatever yadadadada</p>
+    </Book>
+    <Book title={secondBook.title} author={secondBook.author} publisher={secondBook.publisher}/>
+    <Book/>
+    <Book/>
+    </section>
+  );
+}
+
+const Book = ({title, author, publisher, job, children}) => {
+  // const {img, title, author} = properties;
+  return (
+    <article>
+      <h1> {title}</h1>
+      <h4>{author}</h4>
+      <p>{job}</p>
+      <p>{publisher}</p>
+      {children}
+    </article>
+  )
+}
+
+
+ReactDom.render(<BookList/>, document.getElementById('root'));
