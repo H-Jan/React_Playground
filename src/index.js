f6f09b20d88d95c9803edb21bc459e2703bbe2a4
Hani J <hanijandali@gmail.com>
2022-07-22 23:20:00 -0700

    Event handlers within components

--- src/index.js
+++ src/index.js
@@ -3,43 +3,51 @@ import ReactDom from 'react-dom';
 import './index.css'
 
 //Set Up variables
+// set up variables as an array
 
-const firstBook = {
-  author: 'Amelia Airheart',
-  title: 'I love You to the moon and back',
-  publisher: 'Penguin House Publishing',
-  job:'Writer'
-}
-
-const secondBook = {
-  author: 'F Scott Fitz',
-  title: 'Summers Paradise',
-  publisher: 'Penguin House Publishing',
-  job:'Artist'
-}
+const books = [
+  {
+    id: 1,
+    author: 'Amelia Airheart',
+    title: 'I love You to the moon and back',
+    publisher: 'Penguin House Publishing',
+    job:'Writer'
+  }, 
+  {
+    id: 2,
+    author: 'F Scott Fitz',
+    title: 'Summers Paradise',
+    publisher: 'Penguin House Publishing',
+    job: 'Artist'
+  }
+];
 
 function BookList(){
   return (
-    <section className='booklist'>
-    <Book title={firstBook.title} author={firstBook.author}>
-      <p> Lorem ipsum dolor sit amet whatever yadadadada</p>
-    </Book>
-    <Book title={secondBook.title} author={secondBook.author} publisher={secondBook.publisher}/>
-    <Book/>
-    <Book/>
-    </section>
+    <section className='booklist'>{books.map((book) => {
+      return (
+        <Book key={book.id} {...book}>
+        </Book>
+      )
+    })}
+    </section> 
   ); 
 }
 
-const Book = ({title, author, publisher, job, children}) => {
-  // const {img, title, author} = properties;
+const Book = (properties) => {
+  const {title, author, job, publisher} = properties;
+  // attribute, eventHandler
+  // onClick, onMouseOver
+  const clickHandler = () => {
+    alert('hello world');
+  }
   return (
     <article>
       <h1> {title}</h1>
       <h4>{author}</h4>
       <p>{job}</p>
       <p>{publisher}</p>
-      {children}
+      <button type="button" onClick={clickHandler}> Reference Example</button>
     </article>
   )
 }
